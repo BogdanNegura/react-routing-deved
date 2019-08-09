d584956cdc66732e416406a32b92fa43a0606860
Bogdan Negura <id.bogdann@yahoo.com>
2019-08-09 23:57:04 +0100

    create the components

---
+++ src/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Nav() {
+  return (
+    <div>
+        <h3>logo</h3>
+        <nav>
+            <ul>
+                <li>About</li>
+                <li>Shop</li>
+            </ul>
+        </nav>
+    </div>
+  );
+}
+
+export default Nav;
