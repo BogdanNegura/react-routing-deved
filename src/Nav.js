02b57bded6da45df05e8348f0bf2b6457797efca
Bogdan Negura <id.bogdann@yahoo.com>
2019-08-11 17:31:02 +0100

    use Dinamic route and fetch data to render  the the name and images of items

--- src/Nav.js
+++ src/Nav.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom'
 
 function Nav() {
+  const navStyle = {
+    color: 'white'
+  };
   return (
-    <div>
+    <nav>
         <h3>logo</h3>
-        <nav>
-            <ul>
-                <li>About</li>
-                <li>Shop</li>
-            </ul>
-        </nav>
-    </div>
+        <ul className="nav-links">
+            <Link style={navStyle} to="/about">
+              <li>About</li>
+            </Link>
+            <Link style={navStyle} to="/shop">
+              <li>Shop</li>
+            </Link>
+
+        </ul>
+          
+    </nav>
   );
 }
 
